a306b15cae1d287367292dcc4d9f781e662eb764
Peli, Ferenc <ferenc.peli@accenture.com>
2020-05-04 21:41:49 +0200

    Implement bottom navigation between screens

---
+++ flutter_demo_app/lib/ui/flags/flags_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FlagsScreen extends StatefulWidget {
+  @override
+  _FlagsScreenState createState() => _FlagsScreenState();
+}
+
+class _FlagsScreenState extends State<FlagsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Flags screen"),
+      ),
+    );
+  }
+}
---
+++ flutter_demo_app/lib/ui/main_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import 'flags/flags_screen.dart';
+import 'form/form_screen.dart';
+import 'location/location_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  final RouteObserver<PageRoute> routeObserver;
+
+  MainScreen(this.routeObserver);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MainScreenState();
+  }
+}
+
+class _MainScreenState extends State<MainScreen> {
+  PageController _pageController;
+  int _page = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = new PageController(
+      initialPage: _page,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        children: [
+          Container(child: FlagsScreen()),
+          Container(child: FormScreen()),
+          Container(child: LocationScreen())
+        ],
+        controller: _pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flag),
+            title: Text("Flags"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.playlist_add_check),
+            title: Text("Form"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_city),
+            title: Text("Location"),
+          ),
+        ],
+        onTap: navigationTapped,
+        currentIndex: _page,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  void navigationTapped(int page) {
+    _pageController.animateToPage(
+      page,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.ease,
+    );
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      this._page = page;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+}
